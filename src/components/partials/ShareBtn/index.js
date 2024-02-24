caea4cf94b7eb6154f9ce6820aa1ac18e8351c39
TheLeon9 <leonfall09@gmail.com>
2024-02-24 19:01:12 +0100

    Ajout de la phrase en bas à gauche, ajout du bouton shared en haut à droite

--- src/components/partials/ShareBtn/index.js
+++ src/components/partials/ShareBtn/index.js
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
 import style from './index.module.scss';
 
+import CloseShare from 'p/img/share_img/close_share.svg';
+import LogoGitHub from 'p/img/share_img/logo_github.svg';
+import LogoLinkedin from 'p/img/share_img/logo_linkedin.svg';
+import LogoShare from 'p/img/share_img/logo_share.svg';
+
 const ShareBtn = () => {
+  const [shareOpen, setShareOpen] = useState(false);
+  const [logoPath, setLogoPath] = useState('');
+
+  useEffect(() => {
+    if (shareOpen) {
+      setLogoPath(CloseShare.src);
+    } else {
+      setLogoPath(LogoShare.src);
+    }
+  }, [shareOpen]);
+
+  const handleShareClicked = () => {
+    setShareOpen(!shareOpen);
+  };
+
   return (
     <div className={style.share_btn_cont}>
-      <div>
-        <p>Non</p>
-      </div>
+      <button
+        onClick={handleShareClicked}
+        className={`${style.btn_share} ${
+          shareOpen ? style.btn_share_close : style.btn_share_open
+        }`}
+      >
+        <Image src={logoPath} alt="Logo Share" width={20} height={20} />
+      </button>
+      <Link href="https://github.com/TheLeon9" target="_blank">
+        <button
+          className={`${style.btn_github} ${
+            shareOpen ? style.btn_github_close : style.btn_github_open
+          }`}
+        >
+          <Image
+            src={LogoGitHub.src}
+            alt="Logo GitHub"
+            width={20}
+            height={20}
+          />
+        </button>
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/florian-moracchini/"
+        target="_blank"
+      >
+        <button
+          className={`${style.btn_link} ${
+            shareOpen ? style.btn_link_close : style.btn_link_open
+          }`}
+        >
+          <Image
+            src={LogoLinkedin.src}
+            alt="Logo Linkedin"
+            width={20}
+            height={20}
+          />
+        </button>
+      </Link>
     </div>
   );
 };
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ function Document() {
       <Head>
         {/* FONTS */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Orbitron:wght@400..900&display=swap"
           rel="stylesheet"
