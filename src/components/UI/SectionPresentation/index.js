35f964e4a084e11615d3e667482de69e9c0d99a1
TheLeon9 <leonfall09@gmail.com>
2024-04-21 14:01:19 +0200

    Text Home Animation

--- src/components/UI/SectionPresentation/index.js
+++ src/components/UI/SectionPresentation/index.js
@@ -1,77 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import style from './index.module.scss';
-import { gsap } from 'gsap';
 
 const SectionPresentation = (props) => {
-  const [isDev, setIsDev] = useState(true);
-
-  useEffect(() => {
-    const spanArray = Array.from(document.querySelectorAll('.span_rotate'));
-
-    let indexDelay = 1;
-    const indexDelayAdd = 0.1;
-    const animDuration = 0.5;
-
-    const animate = () => {
-      spanArray.forEach((singleSpan, index) => {
-        gsap.from(singleSpan, {
-          delay: indexDelay,
-          duration: animDuration,
-          rotationY: 360,
-          ease: 'power2.out',
-          onUpdate: changeLetters(index + 1),
-          onComplete: () => {
-            if (index === spanArray.length - 1) {
-              indexDelay = 0;
-              console.log('là aussi');
-              setIsDev(!isDev);
-              setTimeout(animate, 2000);
-            }
-          },
-        });
-        indexDelay += indexDelayAdd;
-      });
-    };
-
-    const changeLetters = (index) => {
-      if (index > 2) {
-        let lettersToChange = [];
-        if (isDev) {
-          lettersToChange = [
-            { selector: '.span_rotate:nth-child(3)', letter: 'v' },
-            { selector: '.span_rotate:nth-child(4)', letter: 'e' },
-            { selector: '.span_rotate:nth-child(5)', letter: 'l' },
-            { selector: '.span_rotate:nth-child(6)', letter: 'o' },
-            { selector: '.span_rotate:nth-child(7)', letter: 'p' },
-            { selector: '.span_rotate:nth-child(8)', letter: 'e' },
-            { selector: '.span_rotate:nth-child(9)', letter: 'r' },
-          ];
-        } else if (!isDev) {
-          lettersToChange = [
-            { selector: '.span_rotate_cont:nth-child(3)', letter: 's' },
-            { selector: '.span_rotate_cont:nth-child(4)', letter: 'i' },
-            { selector: '.span_rotate_cont:nth-child(5)', letter: 'g' },
-            { selector: '.span_rotate_cont:nth-child(6)', letter: 'n' },
-            { selector: '.span_rotate_cont:nth-child(7)', letter: 'e' },
-            { selector: '.span_rotate_cont:nth-child(8)', letter: 'r' },
-            { selector: '.span_rotate_cont:nth-child(9)', letter: '' },
-          ];
-        }
-        lettersToChange.forEach(({ selector, letter }) => {
-          gsap.to(selector, { duration: 0.5, textContent: letter, delay: 0.1 });
-        });
-      }
-    };
-
-    animate();
-  });
-
-  // useEffect(() => {
-  //   // animate();
-  //   setLaunchAnim(!launchAnim);
-  //   // setTimeout(animate, 2000);
-  // }, [isDev]);
-
   return (
     <section className={style.section_presentation_cont}>
       <h1 className={style.presentation_title}>
@@ -80,19 +10,17 @@ const SectionPresentation = (props) => {
           {props.presentationTitleColored}
         </span>
       </h1>
-      <p className={style.span_text_cont}>
+      <p className={`p_big ${style.span_text_cont}`}>
         {props.presentationText === 'Home' ? (
           <>
-            Creative&nbsp;Web&nbsp;
-            <span className={`span_rotate`}>D</span>
-            <span className={`span_rotate`}>e</span>
-            <span className={`span_rotate`}>v</span>
-            <span className={`span_rotate`}>e</span>
-            <span className={`span_rotate`}>l</span>
-            <span className={`span_rotate`}>o</span>
-            <span className={`span_rotate`}>p</span>
-            <span className={`span_rotate`}>e</span>
-            <span className={`span_rotate`}>r</span>
+            Creative&nbsp; Web&nbsp; &nbsp;
+            <span className={style.span_animation_cont}>
+              <span className={style.span_hook}>[</span>
+              <span className={style.span_developer}>Developer</span>
+              <span className={style.span_designer}>Designer</span>
+              <span className={style.span_hook}>]</span>
+            </span>
+            &nbsp;
           </>
         ) : (
           <span>{text}</span>
